65eba2383928bf5508b59e08dc7eb0407b6d1024
Jozo Krstanovic <jkrstanovic@hrcloud.corp>
2023-06-01 15:00:40 +0200

    Initial backend commit

---
+++ backend/Program.cs
@@ -0,0 +1,39 @@
+using F1StatsServer.Data;
+using F1StatsServer.Interface;
+using F1StatsServer.Model;
+using F1StatsServer.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AdventureContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
