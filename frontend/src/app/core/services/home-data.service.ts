1e5c29e18e504cb0730bb116a85857c14cc16781
jozo-krstanovic <jozo.molten@gmail.com>
2023-07-05 13:16:34 +0200

    Added more data fetching and display on main page

--- frontend/src/app/core/services/home-data.service.ts
+++ frontend/src/app/core/services/home-data.service.ts
@@ -1,16 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { grandPrix } from 'app/shared/models/grandPrix';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HomeDataService {
   baseUrl = 'https://localhost:44347/api/';
+
   constructor(private http: HttpClient) {}
 
   public fetchData(): Observable<grandPrix[]> {
     return this.http.get<grandPrix[]>(this.baseUrl + 'GrandPrix/homepage');
   }
+
 }
--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
 import {
   faPlus,
   faSearch,
@@ -6,28 +7,33 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { HomeDataService } from 'app/core/services/home-data.service';
 import { grandPrix } from 'app/shared/models/grandPrix';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   faPlus = faPlus;
   faSearch = faSearch;
   faStopwatch = faStopwatch;
 
-  isDataLoaded: boolean = false;
-  grandPrixes: Array<grandPrix> = new Array<grandPrix>();
+  isDataLoaded: boolean = true;
+  grandPrixData: Array<grandPrix> = new Array();
+  popularLeagues: Array<grandPrix> = new Array();
 
-  constructor(private homeDataService: HomeDataService) {
+  constructor(private homeDataService: HomeDataService) {}
+
+  ngOnInit(): void {
     this.homeDataService.fetchData().subscribe((data) => {
-      this.grandPrixes = data;
-      this.isDataLoaded = true;
-    });
+      this.grandPrixData = data;
+      this.popularLeagues = this.grandPrixData.slice(0,3);
+    })
   }
+
   openRace() {
     console.log('Klik na cijeli red result-row.');
   }
+
 }
