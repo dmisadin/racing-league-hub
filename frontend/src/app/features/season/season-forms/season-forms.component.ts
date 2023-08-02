02f198dd719b5069f372309d306ae687496b8d8c
dmisadin <dominik.misadin@gmail.com>
2023-08-02 09:56:22 +0200

    Minor update and rebase from Main branch
    Changed Season component to display Points Distribution from database.

--- frontend/src/app/features/season/season-forms/season-forms.component.ts
+++ frontend/src/app/features/season/season-forms/season-forms.component.ts
@@ -11,88 +11,88 @@ import { firstValueFrom } from 'rxjs';
 
 
 @Component({
-	selector: 'app-season-forms',
-	templateUrl: './season-forms.component.html',
-	styleUrls: ['./season-forms.component.scss']
+    selector: 'app-season-forms',
+    templateUrl: './season-forms.component.html',
+    styleUrls: ['./season-forms.component.scss']
 })
 export class SeasonFormsComponent {
-	faArrowLeft = faArrowLeft;
-	faArrowRight = faArrowRight;
+    faArrowLeft = faArrowLeft;
+    faArrowRight = faArrowRight;
 
-	currentStep = 1;
-	totalSteps = 8;
-	preview = '';
-	isSubmitted: boolean = false;
-	stepFormNames = ["info", "qualPoints", "sprintPoints", "racePoints", "fastestLapPoints", "lobbySettings", "assists"]
-	infoValue = { name: "", gameId: 5, platformId: 1, lapsRequiredPercentage: 90 };
+    currentStep = 1;
+    totalSteps = 8;
+    preview = '';
+    isSubmitted: boolean = false;
+    stepFormNames = ["info", "qualPoints", "sprintPoints", "racePoints", "fastestLapPoints", "lobbySettings", "assists"]
+    infoValue = { name: "", gameId: 5, platformId: 1, lapsRequiredPercentage: 90 };
 
-	qualPointsArray = this.fb.array([]);
-	sprintPointsArray = this.fb.array([]);
-	racePointsArray = this.fb.array([]);
-	fastestLapPointsArray = this.fb.array([]);
+    qualPointsArray = this.fb.array([]);
+    sprintPointsArray = this.fb.array([]);
+    racePointsArray = this.fb.array([]);
+    fastestLapPointsArray = this.fb.array([]);
 
-	seasonForm = this.fb.group({
-		// The rest of form group is added inside child components with parent.addControl()
-	});
+    seasonForm = this.fb.group({
+        // The rest of form group is added inside child components with parent.addControl()
+    });
 
-	constructor(private fb: FormBuilder, private addSeasonService: AddSeasonService) { }
+    constructor(private fb: FormBuilder, private addSeasonService: AddSeasonService) { }
 
-	onSubmit(): void {
-		this.isSubmitted = true;
-		console.log("Submitted form: ", this.seasonForm.value, this.seasonForm.valid, this.seasonForm);
-    var info : Info = this.seasonForm.get('info')!.value;
-    var fastestLapPoints : Point[] = this.seasonForm.get('fastestLapPoints')!.value;
+    onSubmit(): void {
+        this.isSubmitted = true;
+        console.log("Submitted form: ", this.seasonForm.value, this.seasonForm.valid, this.seasonForm);
+        var info: Info = this.seasonForm.get('info')!.value;
+        var fastestLapPoints: Point[] = this.seasonForm.get('fastestLapPoints')!.value;
 
 
-    var seasonInsert : SeasonInsert;
-    seasonInsert = {
-      leagueId : 1,
-      gameId : this.infoValue.gameId,
-      platformId : this.infoValue.platformId,
-      imagePath : '',
-      name : info.name,
-      lapsRequiredPercentage : info.lapsRequiredPercentage,
-      racePointsDto : this.seasonForm.get('racePoints')!.value,
-      qualPointsDto : this.seasonForm.get('qualPoints')!.value,
-      fastestLapPointDto : fastestLapPoints[0] || { position : 10, points : 1},
-      lobbySettingDto :  this.seasonForm.get('lobbySettings')!.value,
-      assistDto : this.seasonForm.get('assists')!.value
+        var seasonInsert: SeasonInsert;
+        seasonInsert = {
+            leagueId: 1,
+            gameId: this.infoValue.gameId,
+            platformId: this.infoValue.platformId,
+            imagePath: '',
+            name: info.name,
+            lapsRequiredPercentage: info.lapsRequiredPercentage,
+            racePointsDto: this.seasonForm.get('racePoints')!.value,
+            qualPointsDto: this.seasonForm.get('qualPoints')!.value,
+            fastestLapPointDto: fastestLapPoints[0] || { position: 10, points: 1 },
+            lobbySettingDto: this.seasonForm.get('lobbySettings')!.value,
+            assistDto: this.seasonForm.get('assists')!.value
+        }
+        //Add redirect from form to created Season page.
+        console.log(seasonInsert);
+        this.addSeason(seasonInsert);
     }
-		//Add redirect from form to created Season page.
-    console.log(seasonInsert);
-    this.addSeason(seasonInsert);
-	}
 
-  getPropArray(value: string): Point[] {
-    return this.seasonForm.get(value)?.value as Point[];
-  }
+    getPropArray(value: string): Point[] {
+        return this.seasonForm.get(value)?.value as Point[];
+    }
 
-  async addSeason(seasonInsert: SeasonInsert): Promise<void>{
-    const result = await firstValueFrom(this.addSeasonService.addSeason(seasonInsert));
-    console.log(result);
-  }
+    async addSeason(seasonInsert: SeasonInsert): Promise<void> {
+        const result = await firstValueFrom(this.addSeasonService.addSeason(seasonInsert));
+        console.log(result);
+    }
 
-	nextStep() {
-		if (this.currentStep < this.totalSteps)
-			this.currentStep++;
-	}
+    nextStep() {
+        if (this.currentStep < this.totalSteps)
+            this.currentStep++;
+    }
 
-	previousStep() {
-		if (this.currentStep > 1)
-			this.currentStep--;
-	}
+    previousStep() {
+        if (this.currentStep > 1)
+            this.currentStep--;
+    }
 
-	get stepValidity() {
-		return this.seasonForm?.get(this.stepFormNames[this.currentStep - 1])?.valid;
-		// This can be improved by checking if all the steps before it are valid as well.
-		// But right now, you can't skip the steps, so it doesn't mater until something like nonlinear "Material Stepper" is implemented
-	}
+    get stepValidity() {
+        return this.seasonForm?.get(this.stepFormNames[this.currentStep - 1])?.valid;
+        // This can be improved by checking if all the steps before it are valid as well.
+        // But right now, you can't skip the steps, so it doesn't mater until something like nonlinear "Material Stepper" is implemented
+    }
 
-	getControl(name: string): FormControl {
-		return this.seasonForm.get(name) as FormControl;
-	}
+    getControl(name: string): FormControl {
+        return this.seasonForm.get(name) as FormControl;
+    }
 
-	checkError(error: string, ...name: string[]) {
-		return name.some(el => this.getControl(el)?.hasError(error));
-	}
+    checkError(error: string, ...name: string[]) {
+        return name.some(el => this.getControl(el)?.hasError(error));
+    }
 }
--- frontend/src/app/features/season/season.component.ts
+++ frontend/src/app/features/season/season.component.ts
@@ -11,10 +11,8 @@ import { Subscription } from 'rxjs';
     styleUrls: ['./season.component.scss']
 })
 export class SeasonComponent {
-
     seasonItem$!: Subscription;
     seasonItem = new Season();
-
     isDataLoaded: boolean = true;
     seasonId: number = 0;
     assists: any;
@@ -55,33 +53,10 @@ export class SeasonComponent {
         this.seasonItem$.unsubscribe();
     }
 
-    mockAssists = [
-        { iconPath: 'assists/tractioncontrol.png', name: 'Traction Control', value: 'Medium' },
-        { iconPath: 'assists/abs.png', name: 'ABS', value: 'Off' },
-        { iconPath: 'assists/racingline.png', name: 'Racing Line', value: 'Corners only' },
-        { iconPath: 'assists/gearbox.png', name: 'gearbox', value: 'Manual' },
-    ];
-
-    mockLobby = [
-        { iconPath: 'lobby/qualifying.png', name: 'Qualifying', value: 'Short' },
-        { iconPath: 'lobby/formationlap.png', name: 'Formation Lap', value: 'On' },
-        { iconPath: 'lobby/equalcarperformance.png', name: 'Race Distance', value: '50%' },
-        { iconPath: 'lobby/weather.png', name: 'Weather', value: 'Dynamic' },
-        { iconPath: 'lobby/cornercutting.png', name: 'Corner cutting', value: 'strict' },
-        { iconPath: 'lobby/damage.png', name: 'Damage', value: 'Standard' },
-        { iconPath: 'lobby/parcferme.png', name: 'Parc Ferme', value: 'On' },
-        { iconPath: 'lobby/equalcarperformance.png', name: 'Engine', value: 'Equal' },
-        { iconPath: 'lobby/safetycar.png', name: 'Safety Car', value: 'Reduced' },
-        { iconPath: 'lobby/start.png', name: 'Starts', value: 'Manual' },
-        { iconPath: 'lobby/collisions.png', name: 'Collisions', value: 'On' },
-        { iconPath: 'lobby/ghosting.png', name: 'Ghosting', value: 'Off' },
-    ]
-
-    mockPoints = [15, 12, 10, 8, 6, 5, 4, 3, 2, 1];
-
     camelCaseToWords(camel: string) {
         return camel.replace(/([A-Z])/g, " $1");
     }
+
     booleanToOnOff(bool: boolean) {
         return bool ? "On" : "Off";
     }
