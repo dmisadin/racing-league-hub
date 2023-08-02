5266724c6a039d32873c0edeaf88d9b1a955e8a9
dmisadin <dominik.misadin@gmail.com>
2023-08-02 13:51:47 +0200

    Started working to table standings with data from database

--- frontend/src/app/shared/models/grandprix/GrandPrix.ts
+++ frontend/src/app/shared/models/grandprix/GrandPrix.ts
@@ -1,7 +1,35 @@
+class Track {
+    id: number = 0;
+    name: string = "";
+    location: string = "";
+    imagePath: string | null = "";
+    countryIso: string = "";
+}
+
+interface SessionResult {
+    driverId: number
+    teamId: number
+    pointsGained: number
+}
+
+interface Team {
+    id: number
+    name: string
+    imagePath: string
+    colorHex: string
+}
+
 export class GrandPrix {
     id?: number = 0;
     seasonId: number = 0;
     name: string = '';
+    startTime: string = '';
     hasSprint: boolean = false;
     youtubeUrl: string = 'youtube.com';
+    fastestDriverId?: number;
+    track!: Track;
+    races: SessionResult[] = [];
+    qualifications: SessionResult[] = [];
+    sprints: SessionResult[] = [];
+    teams: Team[] = [];
 }
