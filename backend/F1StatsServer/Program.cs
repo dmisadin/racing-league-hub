660a3bec41c564797a45b50f22e31e207c34100a
dmisadin <dominik.misadin@gmail.com>
2026-02-17 23:57:13 +0100

    Cleanup project for new controllers and repositories

--- backend/F1StatsServer/Program.cs
+++ backend/F1StatsServer/Program.cs
@@ -1,100 +1,20 @@
 using F1StatsServer.Data;
 using F1StatsServer.Infrastructure;
-using F1StatsServer.Interfaces;
-using F1StatsServer.Repositories;
-using F1StatsServer.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Filters;
-using System.Reflection;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//Add services to the container.
 
 builder.Services.AddControllers()
                 .AddNewtonsoftJson();
 
-//Add generic repository to application scope:
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
 
-//Add specific repositories to application scope:
-builder.Services.AddScoped<IGrandPrixRepository, GrandPrixRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ILeagueRepository, LeagueRepository>();
-builder.Services.AddScoped<ISeasonRepository, SeasonRepository>();
-builder.Services.AddScoped<SessionResultRepository>();
-
-//Add services to application scope:
-builder.Services.AddScoped<ILeagueService, LeagueService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ISeasonService, SeasonService>();
-builder.Services.AddScoped<IGrandPrixService, GrandPrixService>();
-builder.Services.AddScoped<ISessionResultService, SessionResultService>();
-builder.Services.AddScoped<IDriverService, DriverService>();
-
-
-builder.Configuration.AddEnvironmentVariables()
-    .AddUserSecrets(Assembly.GetExecutingAssembly(), true);
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
-});
-builder.Services.AddDbContextPool<AdventureContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetSection("DefaultConnection").Value)
-                   .LogTo(Console.WriteLine, LogLevel.Information);
-});
-
-//Authentication template provided at: https://github.com/patrickgod/JwtWebApiTutorial
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-                .GetBytes(builder.Configuration.GetSection("Token").Value)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
-    policy =>
-    {
-        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
-    }));
-
-
+builder.Services.AddDbContext<AdventureContext>(options => 
+                    options.UseNpgsql(builder.Configuration
+                            .GetConnectionString("DefaultConnection"))
+                            .UseSnakeCaseNamingConvention());
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors("NgOrigins");
 
 app.UseHttpsRedirection();
 
