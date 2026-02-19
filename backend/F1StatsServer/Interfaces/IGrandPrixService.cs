e1a865127197abe6ca5b8aa19158296b27494678
dmisadin <dominik.misadin@gmail.com>
2026-02-19 20:22:03 +0100

    Remove old interfaces

--- backend/F1StatsServer/Interfaces/IGrandPrixService.cs
+++
@@ -1,9 +0,0 @@
-﻿using F1StatsServer.Dto.GrandPrixDtos;
-
-namespace F1StatsServer.Interfaces
-{
-    public interface IGrandPrixService
-    {
-        public Task<int> InsertDataAsync(List<GrandPrixInsertDto> data);
-    }
-}
--- backend/F1StatsServer/Interfaces/ISessionResultService.cs
+++
@@ -1,9 +0,0 @@
-﻿using F1StatsServer.Dto.ResultsDtos;
-
-namespace F1StatsServer.Interfaces
-{
-    public interface ISessionResultService
-    {
-        Task<bool> InsertResultsAsync(List<SessionResultDto> data);
-    }
-}
