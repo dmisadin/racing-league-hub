0ff191f5110976ca5236625684d29737088eff8a
dmisadin <dominik.misadin@gmail.com>
2026-03-09 23:02:52 +0100

    Add home component

--- RacingLeagueHubClient/src/app/app.routes.ts
+++ RacingLeagueHubClient/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '', 
+        loadComponent: () => import('./features/home/home.component').then(c => c.HomeComponent)
+    },
+    {
         path: '**',
         loadComponent: () => import('./core/components/not-found.component').then(c => c.NotFoundComponent)
     }
