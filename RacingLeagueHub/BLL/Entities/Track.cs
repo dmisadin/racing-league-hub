ac0498e7bd9b46d2910661ec8919f48cbbe0d8d3
dmisadin <dominik.misadin@gmail.com>
2026-03-11 15:46:02 +0100

    Add Countries static dictionary

--- RacingLeagueHub/BLL/Entities/Track.cs
+++ RacingLeagueHub/BLL/Entities/Track.cs
@@ -3,7 +3,7 @@
 public class Track : EntityBase
 {
     public string Name { get; set; }
-    public long Country { get; set; }
+    public string Country { get; set; }
     public string City { get; set; }
     public decimal? Elevation { get; set; }
     public string? ShortName { get; set; }
