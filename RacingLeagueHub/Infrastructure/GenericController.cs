a63f52f2ca8af48d8514cb37b206b6292b5a14ce
dmisadin <dominik.misadin@gmail.com>
2026-03-16 16:40:35 +0100

    Fix async and FromRoute in GenericController

--- RacingLeagueHub/Infrastructure/GenericController.cs
+++ RacingLeagueHub/Infrastructure/GenericController.cs
@@ -1,7 +1,7 @@
-﻿using RacingLeagueHub.BLL.Mapping.DtoFactories;
-using RacingLeagueHub.BLL.Entities;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using RacingLeagueHub.BLL.Entities;
+using RacingLeagueHub.BLL.Mapping.DtoFactories;
 using RacingLeagueHub.BLL.Models.Dtos;
 
 namespace RacingLeagueHub.Infrastructure;
@@ -24,18 +24,19 @@ public abstract class GenericController<TEntity, TDto> : Controller
     [HttpGet("get-by-id/{id}")]
     public virtual async Task<ActionResult<TDto>> GetOne([FromRoute] long id)
     {
-        var entity = await repository.FindAsync(id);
+        var dto = await repository.Query()
+                                .Where(e => e.Id == id)
+                                .Select(DtoFactory.ToDtoExpression())
+                                .FirstOrDefaultAsync();
 
-        if (entity == null)
+        if (dto == null)
             return NotFound();
-
-        var dto = DtoFactory.ToDto(entity);
 
         return Ok(dto);
     }
 
     [HttpPost("add")]
-    public virtual async Task<ActionResult<long>> Add(TDto dto)
+    public virtual async Task<ActionResult<long>> Add([FromBody] TDto dto)
     {
         var entity = repository.Create();
         DtoFactory.FromDto(entity, dto);
@@ -46,26 +47,61 @@ public abstract class GenericController<TEntity, TDto> : Controller
         return Ok(entity.Id);
     }
 
-    [HttpPost("update")]
-    public virtual async Task<ActionResult<long>> Update([FromBody] TDto dto, [FromQuery] long id)
+    [HttpPost("update/{id}")]
+    public virtual async Task<ActionResult<long>> Update([FromBody] TDto dto, [FromRoute] long id)
     {
-        var entity = this.repository.FindAsync(id).Result;
+        var entity = await this.repository.FindAsync(id);
 
         if (entity == null)
             return NotFound();
 
         DtoFactory.FromDto(entity, dto);
 
+        await this.repository.CommitAsync();
+
         return Ok(entity.Id);
     }
 
-    [HttpDelete("delete")]
-    public virtual async Task<IActionResult> Delete(long id)
+    [HttpDelete("delete/{id}")]
+    public virtual async Task<IActionResult> Delete([FromRoute] long id)
     {
         var rows = await repository.Query()
             .Where(x => x.Id == id)
             .ExecuteDeleteAsync();
 
         return rows == 0 ? NotFound() : NoContent();
+    }
+
+    [HttpPost("upsert-many")]
+    public async Task UpsertMany([FromBody]IEnumerable<TDto> dtos)
+    {
+        var toUpdate = dtos.Where(x => x.Id > 0).ToList();
+        var toInsert = dtos.Where(x => x.Id == 0).ToList();
+
+        var ids = toUpdate.Select(x => x.Id).ToList();
+
+        var existing = await repository.Query()
+            .Where(e => ids.Contains(e.Id))
+            .ToListAsync();
+
+        foreach (var dto in toUpdate)
+        {
+            var entity = existing.FirstOrDefault(e => e.Id == dto.Id);
+            if (entity != null)
+                DtoFactory.FromDto(entity, dto);
+        }
+
+        var newEntities = new List<TEntity>();
+
+        foreach (var dto in toInsert)
+        {
+            var newEntity = repository.Create();
+            DtoFactory.FromDto(newEntity, dto);
+            newEntities.Add(newEntity);
+        }
+
+        await repository.InsertAsync(newEntities.ToArray());
+
+        await repository.CommitAsync();
     }
 }
