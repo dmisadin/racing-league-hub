796c52fad1ac126bca3b07243b2308a255628bce
dmisadin <dominik.misadin@gmail.com>
2026-04-13 15:07:55 +0200

    Update controllers with paginated content

---
+++ RacingLeagueHub.Api/Controllers/SeasonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RacingLeagueHub.Application.DtoFactories;
+using RacingLeagueHub.Application.Dtos;
+using RacingLeagueHub.Domain.Abstractions;
+using RacingLeagueHub.Domain.Entities.Seasons;
+using RacingLeagueHub.Domain.Infrastructure;
+
+namespace RacingLeagueHub.Api.Controllers;
+
+[Route("api/seasons")]
+public class SeasonController : GenericController<Season, SeasonDto>
+{
+    private readonly ISeasonRepository seasonRepository;
+
+    public SeasonController(IRepository<Season> repository, 
+                            ISeasonRepository seasonRepository) 
+        : base(repository)
+    {
+        this.seasonRepository = seasonRepository;
+    }
+
+    protected override IDtoFactory<Season, SeasonDto> DtoFactory => new SeasonDtoFactory();
+}
