98a27a2f999342a78cefd6e02074b877f5970a89
dmisadin <dominik.misadin@gmail.com>
2026-05-12 23:43:12 +0200

    rename authentication extension class

--- RacingLeagueHub.Api/Startup/AuthenticationServiceExtensions.cs
+++ RacingLeagueHub.Api/Startup/AuthenticationServiceExtensions.cs
@@ -4,7 +4,7 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace RacingLeagueHub.Api.Startup;
 
-public static class AuthStartup
+public static class AuthenticationServiceExtensions
 {
     public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration config)
     {
@@ -14,14 +14,23 @@ public static class AuthStartup
                 options.MapInboundClaims = false;
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
+                    RequireSignedTokens = true,
+                    RequireExpirationTime = true,
+                    
                     ValidateIssuer = true,
                     ValidateAudience = true,
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
+                    
                     ValidIssuer = config["Jwt:Issuer"],
                     ValidAudience = config["Jwt:Audience"],
+                    ValidAlgorithms = ["HS256"],
+                    
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Secret"]!)),
+                    
                     ClockSkew = TimeSpan.Zero,
+                    
+                    NameClaimType = "sub", 
                     RoleClaimType = "role"
                 };
             });
