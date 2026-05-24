ff6f6bbd11f5fffd0b3126b77f9bd65789120f4a
dmisadin <dominik.misadin@gmail.com>
2026-05-25 00:21:46 +0200

    Change design of navbar and homepage

--- RacingLeagueHubClient/src/app/features/home/home.component.ts
+++ RacingLeagueHubClient/src/app/features/home/home.component.ts
@@ -2,15 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'home',
-    template: `
-        <div class="space-y-4">
-            @for(item of [].constructor(50); track $index)
-            {
-            <p>
-                Scrollable content...
-            </p>
-            }
-        </div>`,
+    templateUrl: './home.component.html',
 })
 export class HomeComponent {
 
