b82bdfb50fad20075886dc4cc16440c0a9afa8cc
dmisadin <dominik.misadin@gmail.com>
2026-09-09 23:26:33 +0200

    Remove deprecated repository

--- RacingLeagueHub.Domain/Abstractions/Admin/ITrackLayoutRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using RacingLeagueHub.Domain.Entities;
-using RacingLeagueHub.Domain.Infrastructure;
-
-namespace RacingLeagueHub.Domain.Abstractions.Admin;
-
-public interface ITrackLayoutRepository : IRepository<TrackLayout>
-{
-
-}
